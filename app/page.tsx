6d1dc850749911242c3282be4fe0ec4617355889
Sama <101380876+HasnyS@users.noreply.github.com>
2023-08-02 00:30:21 -0400

    v1-button installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-
+import {Button} from "@/components/ui/button"
 export default function Home() {
   return (
-      <p className={"text-6xl text-red-500"}> Hello SamaBot</p>
+     <Button variant={"ghost"} size={'lg'}>SamaBot</Button>
  )
 }
