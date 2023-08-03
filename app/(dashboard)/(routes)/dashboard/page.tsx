42982b604a06d34bf63a28710619820f99af3621
Sama <101380876+HasnyS@users.noreply.github.com>
2023-08-03 01:30:55 -0400

    added log in buttons

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import {Button} from "@/components/ui/button"
+import {UserButton} from '@clerk/nextjs'
+
 export default function DashBoardPage() {
   return (
+      <div>
      <p> DashBoardPage (protected)</p>
+          <UserButton />
+      </div>
  )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,24 @@
 import Image from 'next/image'
 import {Button} from "@/components/ui/button"
+import Link from "next/link";
+
 export default function LandingPage() {
     return (
         <div>
             Landing Page (Unprotected)
+
+        <div>
+            <Link href={"/sign-in"}>
+            <Button>
+                Login
+            </Button>
+            </Link>
+            <Link href={"/sign-up"}>
+                <Button>
+                    Sign Up
+                </Button>
+            </Link>
+        </div>
         </div>
     )
 }
