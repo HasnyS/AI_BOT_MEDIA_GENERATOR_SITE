50e18dd3d0f4edf6aa59e28bb113e958ce857d14
Sama <101380876+HasnyS@users.noreply.github.com>
2023-08-07 23:12:50 -0400

    sidebar tweaks

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,24 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import {Montserrat} from "next/font/google";
+import {LayoutDashboard} from "lucide-react";
+import {cn} from "@/lib/utils";
 
-const Monserrat
+const poppins= Montserrat({
+    weight:'600',
+    subsets:["latin"]
+});
+
+const routes =[
+    {
+        label:'Dashboard',
+        icon: LayoutDashboard,
+        href: '/dashboard',
+        color: 'text-sky-500',
+    },
+];
+
 
 const Sidebar = () => {
     return (
@@ -20,6 +36,18 @@ const Sidebar = () => {
                         SamaBot
                     </h1>
                 </Link>
+                <div className={'space-y-1'}>
+                    {routes.map}((route) =>(
+                        <Link
+                            href={route.href}
+                            key={route.href}>
+                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                                <div className={'flex items-center flex-1'}>
+                                    <route.icon className={cn('h-5 w-5 mr-3', route.color)}/>
+                                    {route.label}
+                                </div>
+                        </Link>
+                </div>
             </div>
         </div>
     );
