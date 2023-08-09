8991af95f69267a7ea950b86e34fc775e667d4ac
Sama <101380876+HasnyS@users.noreply.github.com>
2023-08-09 01:22:45 -0400

    finished sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,10 @@
-import {Button} from "@/components/ui/button";
-import {Menu} from "lucide-react";
 import {UserButton} from "@clerk/nextjs";
+import MobileSidebar from "@/components/ui/mobile-sidebar";
 
 const Navbar = () => {
   return(
       <div className={"flex items-center p-4"}>
-          <Button variant={"ghost"} size={"icon"} className={"md:hidden"}>
-            <Menu>
-
-            </Menu>
-          </Button>
+        <MobileSidebar/>
           <div className={"flex w-full justify-end"}>
               <UserButton afterSignOutUrl={"/"}/>
           </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,52 +1,86 @@
 "use client";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import {Montserrat} from "next/font/google";
-import {LayoutDashboard} from "lucide-react";
-import {cn} from "@/lib/utils";
+import { Montserrat } from "next/font/google";
+import {CodeIcon, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings2Icon, VideoIcon} from "lucide-react";
+import { cn } from "@/lib/utils";
 
-const poppins= Montserrat({
-    weight:'600',
-    subsets:["latin"]
+const montserrat = Montserrat({
+    weight: "600",
+    subsets: ["latin"],
 });
 
-const routes =[
+const routes = [
     {
-        label:'Dashboard',
+        label: "Dashboard",
         icon: LayoutDashboard,
-        href: '/dashboard',
-        color: 'text-sky-500',
+        href: "/dashboard",
+        color: "text-sky-500",
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/talk",
+        color: "text-violet-500",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/draw",
+        color: "text-pink-700",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/record",
+        color: "text-orange-700",
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        href: "/sing",
+        color: "text-red-500",
+    },
+    {
+        label: "Code Generation",
+        icon: CodeIcon,
+        href: "/invent",
+        color: "text-emerald-500",
+    },
+    {
+        label: "Settings",
+        icon: Settings2Icon,
+        href: "/settings",
+        color: "text-grey-500",
     },
 ];
-
 
 const Sidebar = () => {
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                    <div className="relative w-12 h-8 mr-4">
-                        <Image
-                            fill
-                            alt="logo"
-                            src="/logo.png"
-                        />
+                    <div className="relative w-8 h-8 mr-3">
+                        <Image fill alt="logo" src="/logo.png" />
                     </div>
-                    <h1 className={"text-2xl font-bold"}>
+                    <h1 className={cn("text-2xl font-bold", montserrat.className)}>
                         SamaBot
                     </h1>
                 </Link>
-                <div className={'space-y-1'}>
-                    {routes.map}((route) =>(
+                <div className="space-y-1">
+                    {routes.map((route) => (
                         <Link
                             href={route.href}
-                            key={route.href}>
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
-                                <div className={'flex items-center flex-1'}>
-                                    <route.icon className={cn('h-5 w-5 mr-3', route.color)}/>
-                                    {route.label}
-                                </div>
+                            key={route.href}
+                            className={'text-sm group flex p-3 w-full justify-start font-medium curosr-pointer gover:text-white hover:bg-white/10 rounded-lg transition '}
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                                {route.label}
+                            </div>
                         </Link>
+                    ))}
                 </div>
             </div>
         </div>
