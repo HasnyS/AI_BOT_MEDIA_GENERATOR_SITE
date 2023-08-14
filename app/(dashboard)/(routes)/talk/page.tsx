3f78c54feb21859b190d13b7f5dfd46746ebfeb8
Sama <101380876+HasnyS@users.noreply.github.com>
2023-08-13 23:23:02 -0400

    finished talk mode

--- app/(dashboard)/(routes)/talk/page.tsx
+++ app/(dashboard)/(routes)/talk/page.tsx
@@ -107,11 +107,11 @@ const TalkPage = () => {
                             <div
                                 key={message.content}
                                 className={cn("p-8 w-full flex items-start gap-x-8 rounded-lg",
-                                message.role === "user" ? "bg-white border border-black/10" : "bg-muted"
+                                message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
                                 )}
                             >
                                 {message.role === "user" ? <UserAvatar/> : <BotAvatar/>}
-                                <p =className={'text-sm'}>
+                                <p className='text-sm'>
                                     {message.content}
                                 </p>
                             </div>
